1dcb72a7c98c1c2539e9d6a8b608b2478d0c2888
Kateryna Avramenko <kavramenko2000@gmail.com>
2023-01-27 19:12:20 +0100

    Initial Commit

---
+++ iDresser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iDresser
+//
+//  Created by Kateryna Avramenko on 27.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
