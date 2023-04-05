1aa6852dc477bd5b3202483d0b652eb525f29695
Kateryna Avramenko <kavramenko2000@gmail.com>
2023-04-05 10:51:07 +0300

    Initial Commit

---
+++ iDresser/iDresserApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDresserApp.swift
+//  iDresser
+//
+//  Created by Kateryna Avramenko on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDresserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
